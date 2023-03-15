bd8cfd05cf9fd2cb42f9be52f30ecef24bc9c8b0
Denis <den.dvornikov.88@inbox.ru>
2023-03-15 09:04:16 +0600

    Выполнил задание 1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,6 +1,11 @@
 ﻿Console.Clear();
 System.Console.WriteLine("Введите трезначное число");
 int a = Convert.ToInt32(Console.ReadLine());
+if(a < 1000 && a > 99){
 a= a/10;
 a= a % 10;
 System.Console.WriteLine(a);
+}
+else{
+    System.Console.WriteLine("Не является трехзначным числом");
+}
