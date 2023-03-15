93c6717e5cb797acb2f2aedf0bfa6586f5eac605
Denis <den.dvornikov.88@inbox.ru>
2023-03-15 09:08:46 +0600

    Выполнил Задание 3

---
+++ task3/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+System.Console.WriteLine("Введите день недели в виде числа от 1 до 7");
+int a = Convert.ToInt32(Console.ReadLine());
+switch (a){
+    case 1:
+    System.Console.WriteLine("Не выходной");
+    break;
+    case 2:
+    System.Console.WriteLine("Не выходной");
+    break;
+    case 3:
+    System.Console.WriteLine("Не выходной");
+    break;
+    case 4:
+    System.Console.WriteLine("Не выходной");
+    break;
+    case 5:
+    System.Console.WriteLine("Не выходной");
+    break;
+    case 6:
+    System.Console.WriteLine("Выходной");
+    break;
+    case 7:
+    System.Console.WriteLine("Выходной");
+    break;
+    default:
+    System.Console.WriteLine("Не является днем недели");
+    break;
+}
