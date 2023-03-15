8674e079b45b1a52a9b29fd46cd4ea7b1da29086
Denis <den.dvornikov.88@inbox.ru>
2023-03-15 08:58:51 +0600

    Второе задание выполнено

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+while(number>999){
+    number=number/10;
+}
+if(number<100){
+    System.Console.WriteLine("У числа нет третей цифры");
+}
+else{
+    number=number%10;
+    System.Console.WriteLine($"Третья цифра {number}");
+}
